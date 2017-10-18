91cbe52e816eb5a7edccfc8160260c91193c2e58
Andrés Buitrago <and.buitra@gmail.com>
2017-10-18 08:22:27 -0500

    Boilerplate de las rutas Este commit contiene información muy básica de las rutas que la aplicación utilizará principalmente las cuales son la raíz, el panel de administración y el panel de autenticación. Las rutas internas de la aplicación no están descritas aquí

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/welcome', function () {
-    return view('welcome');
+Route::get('/', function () {
+    return 'If not logged in, index should redirect to login page';
+});
+
+Route::get('/admin', function(){
+    return 'Admin panel. Only accesible for admins. Should redirect to index if logged user is not admin or if user is not logged in';
+});
+
+Route::get('/login', function(){
+    return 'Login panel. Public accessible. Redirected from index and admin if no session is available';
 });
